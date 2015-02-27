b143751dc84371bb482f4e02e5ca084f3d460ca1
Cody Redmond <codydjango@gmail.com>
2015-02-26 19:10:35 -0800

    First test

--- multifilefield/tests.py
+++ multifilefield/tests.py
@@ -1,1 +1,18 @@
-# No tests yet.
+from django import forms
+
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from multifilefield import ClearableFilesField
+
+
+
+class ClearableFilesFieldTest(TestCase):
+    def test_init(self):
+        """Test that initializing the field doesn't break."""
+
+        clearable_files_field = ClearableFilesField(
+            label='Upload files',
+            required=False)
+
+        #self.assertEqual()

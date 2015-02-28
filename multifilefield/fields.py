5f61696b2de1a3eb48fa3d170257896cb4f0223e
Cody Redmond <codydjango@gmail.com>
2015-02-27 20:21:03 -0800

    add validation for maximum number of uploaded files

--- multifilefield/fields.py
+++ multifilefield/fields.py
@@ -2,12 +2,16 @@ import math, floppyforms as forms
 
 from django.core.exceptions import ValidationError
 
-from multifilefield.models import UploadedFile
 from multifilefield.widgets import ClearableFilesWidget, ClearCheckboxSelectMultipleWidget, FilesInputWidget
 
 
 class FilesField(forms.FileField):
+    """This field is a modified file field that handles
+    multiple files rather than a single file."""
+
     widget = FilesInputWidget
+
+
     default_error_messages = {
         'min_num': u'No less than %(min_num)s files uploaded at a time, please (received %(num_files)s).',
         'max_num': u'No more than %(max_num)s files uploaded at a time, please (received %(num_files)s).',
@@ -16,9 +20,9 @@ class FilesField(forms.FileField):
 
 
     def __init__(self, *args, **kwargs):
-        self.min_num = kwargs.pop('min_num', 0)
-        self.max_num = kwargs.pop('max_num', None)
-        self.maximum_file_size = kwargs.pop('maximum_file_size', None)
+        self.min_num        = kwargs.pop('min_num', 0)
+        self.max_num        = kwargs.pop('max_num', None)
+        self.max_file_size  = kwargs.pop('max_file_size', None)
 
         super(FilesField, self).__init__(*args, **kwargs)
 
@@ -50,69 +54,91 @@ class FilesField(forms.FileField):
     def validate(self, data):
         super(FilesField, self).validate(data)
 
-        num_files = len(data)
-        if len(data) and not data[0]:
+        uploaded_files = data
+        num_files = len(uploaded_files)
+        if len(uploaded_files) and not uploaded_files[0]:
             num_files = 0
         if num_files < self.min_num:
             raise ValidationError(self.error_messages['min_num'] % {'min_num': self.min_num, 'num_files': num_files})
         elif self.max_num and num_files > self.max_num:
             raise ValidationError(self.error_messages['max_num'] % {'max_num': self.max_num, 'num_files': num_files})
+        elif self.total_num and
 
-        if self.maximum_file_size:
-            for uploaded_file in data:
-                if uploaded_file.size > self.maximum_file_size:
+        if self.max_file_size:
+            for uploaded_file in uploaded_files:
+                if uploaded_file.size > self.max_file_size:
                     raise ValidationError(self.error_messages['file_size'] % {
                         'uploaded_file_name': uploaded_file.name,
-                        'max_size': self.humanize(self.maximum_file_size)})
+                        'max_size': self.humanize(self.max_file_size)})
 
 
 
 class ClearableFilesField(forms.MultiValueField):
     def __init__(self, *args, **kwargs):
-        kwargs['required'] = False
 
-        label = kwargs.pop('label')
-        label = 'Attach files'
+        default_error_messages = {
+            'total_num': u'No more than %(total_num)s files in total, please (tried %(attempt_num)s).',
+        }
 
-        self.uploaded_original = kwargs.pop('uploaded', [])
-        self.uploaded_files = []
+        required        = kwargs.pop('required', False)
+        label           = kwargs.pop('label', 'Attach files')
+        max_file_size   = kwargs.pop('max_file_size', 1024*1024*5)
+        max_num_files   = kwargs.pop('max_num_files', 5)
+        min_num_files   = kwargs.pop('min_num_files', 0)
 
-        for upload in self.uploaded_original:
-            self.uploaded_files.extend(UploadedFile.objects.filter(id=upload))
+        self.max_num_uploaded   = kwargs.pop('max_num_uploaded', None)
+        self.FileCls            = kwargs.pop('model')
+        self.queryset           = kwargs.pop('queryset', [])
 
-
-        choices = [(uploaded_file.id, uploaded_file) for uploaded_file in self.uploaded_files]
-        self.files = choices
-        self.choices = choices
 
         upload_field = FilesField(
                 label = None,
-                max_num = 5,
-                min_num = 0,
-                maximum_file_size = 1024*1024*5,
-                *args,
-                **kwargs)
+                max_num = max_num_files,
+                min_num = min_num_files,
+                max_file_size = max_file_size)
 
 
         clear_field = forms.MultipleChoiceField(
                 label = None,
-                choices = choices,
-                widget = ClearCheckboxSelectMultipleWidget,
-                *args,
-                **kwargs)
+                choices = self.get_file_choices(),
+                widget = ClearCheckboxSelectMultipleWidget)
+
 
         fields = [upload_field, clear_field]
         widgets = [upload_field.widget, clear_field.widget]
+        widget = ClearableFilesWidget(widgets = widgets)
+
 
         super(ClearableFilesField, self).__init__(
                 label = label,
-                widget = ClearableFilesWidget(widgets = widgets),
-                fields = fields,
-                *args,
-                **kwargs)
+                widget = widget,
+                fields = fields)
+
+
+    def get_file_choices(self):
+        return [(file_obj.id, file_obj) for file_obj in self.queryset]
+
+
+    def validate(self, data_list):
+        if self.max_num_uploaded and data_list:
+            files_to_upload = data_list[0]
+            files_to_delete = data_list[1]
+
+            files_total = len(list(self.queryset))
+            files_total -= len(files_to_delete)
+            files_total += len(files_to_upload)
+
+            if files_total > self.max_num_uploaded:
+                raise ValidationError(self.error_messages['total_num'] % {
+                    'total_num': max_num_uploaded,
+                    'attempt_num': files_total})
 
 
     def compress(self, data_list):
+        """ This field returns a tuple that expresses the original list
+        of uploaded file objects, a second list of new files objects to upload
+        and a third list of file objects to delete."""
+
         files_to_upload = []
         files_to_delete = []
 
@@ -120,4 +146,4 @@ class ClearableFilesField(forms.MultiValueField):
             files_to_upload = data_list[0]
             files_to_delete = data_list[1]
 
-        return (self.uploaded_original, files_to_upload, files_to_delete)
+        return (self.queryset, files_to_upload, files_to_delete)
